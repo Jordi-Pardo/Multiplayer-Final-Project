344d2265a485d60e9edb7e7fe35a19d406f15120
Jordi Pardo <jordipardogutierrez@gmail.com>
2022-01-12 22:50:53 +0100

    Destructible Object

--- MultiplayerFighter_XO/Assets/Destructible.cs
+++ MultiplayerFighter_XO/Assets/Destructible.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public interface IDamageable
 {
     public void RecieveDamage();
 }
 
+[RequireComponent(typeof(PhotonView))]
 public class Destructible : MonoBehaviour,IDamageable
 {
     public void RecieveDamage()
     {
-
-            Destroy(gameObject);
-       
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("DestroyObject", RpcTarget.All);
     }
 
+    [PunRPC]
+    public void DestroyObject()
+    {
+        Destroy(gameObject,0.2f);
+    }
 }
